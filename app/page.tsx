fb5d566b6ba93e35eaf493794b8206bbf091e285
Prince Jain <pj122333444@gmail.com>
2025-04-20 11:48:24 +0530

    basic api and fetch setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>welcome, to the second commit</h1>;
+  return <h1 className="text-zinc-800">welcome, to the second commit</h1>;
 }
