ed41512037a1ddb22d46042d96a9862298ed68ad
Prince Jain <pj122333444@gmail.com>
2025-05-20 23:45:44 +0530

    analytics page half done

--- app/(routes)/analytics/page.jsx
+++ app/(routes)/analytics/page.jsx
@@ -1,3 +1,20 @@
 export default async function AnalyticsPage() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <section className="min-h-screen flex flex-col items-center gap-4 bg-gradient-to-b from-blue-900/20 via-transparent to-transparent">
+          <div className="flex justify-center items-center gap-1 rounded-full bg-blue-500/10 backdrop-blur-sm py-2 px-4 mt-20 mb-4">
+            <div className="h-1 w-1 bg-blue-500 rounded-full"></div>
+            <p className="text-blue-400 text-xs">Email Tracking</p>
+          </div>
+          <div className="text-2xl sm:text-3xl font-bold flex flex-col items-center text-center">
+            <h1>Email Analytics</h1>
+          </div>
+          <h1 className="text-lg text-white/70 max-w-2xl text-center mt-2">
+            Track and analyze all your scheduled emails in one place
+          </h1>
+        </section>
+      </div>
+    </>
+  );
 }
